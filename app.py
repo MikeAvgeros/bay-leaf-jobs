d1525e5036bad00865cb5fb8f7ef960892827a74
Mike Avgeros <mike.avgeros@gmail.com>
2021-08-17 14:35:10 +0000

    Create Flask Application

---
+++ app.py
@@ -0,0 +1,41 @@
+# -------- Imports and configuration --------
+import os
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from functools import wraps
+from werkzeug.security import generate_password_hash, check_password_hash
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+# mongo = PyMongo(app)
+
+
+# ------------ Index / homepage -------------
+@app.route("/")
+@app.route("/home")
+@app.route("/index")
+def home():
+    return "Homepage"
+
+
+# --------------- About page ----------------
+@app.route("/about")
+def about():
+    return "Aboutpage"
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
+

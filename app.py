4e3ad1b2180b2df2592383609ec2edb73a9cb047
Mike Avgeros <mike.avgeros@gmail.com>
2021-08-20 22:07:13 +0100

    Add navbar and footer. Create relevant html page for the project.

--- app.py
+++ app.py
@@ -13,14 +13,16 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
+
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
+
 mongo = PyMongo(app)
 
 
-# ------------ Index / homepage -------------
+# ------------ Index / Home page -------------
 @app.route("/")
 @app.route("/home")
 @app.route("/index")

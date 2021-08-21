e3671adc0d2f562dbb394acaf38be507fa86a9c7
Mike Avgeros <mike.avgeros@gmail.com>
2021-08-21 20:21:55 +0100

    Add profile page. Update home page styling.

--- app.py
+++ app.py
@@ -111,6 +111,14 @@ def register():
     return render_template("register.html")
 
 
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab the session user's username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),

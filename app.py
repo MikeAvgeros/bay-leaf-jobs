955fc0c0e33e86eef8494addd96afd6110a8dd2b
Mike Avgeros <mike.avgeros@gmail.com>
2021-08-21 18:50:15 +0100

    Add login and register functionality. Create footer. Add search bar on home page. Update links

--- app.py
+++ app.py
@@ -35,10 +35,74 @@ def home():
     return render_template("index.html")
 
 
+# --------------- Jobs page ----------------
+@app.route("/jobs")
+def jobs():
+    return render_template("jobs.html")
+
+
 # --------------- About page ----------------
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+# --------------- Contact page ----------------
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+# --------------- Login page ----------------
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
+# --------------- Register page ----------------
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+
+    return render_template("register.html")
 
 
 if __name__ == "__main__":

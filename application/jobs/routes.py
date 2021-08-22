be3c07c1fc615c3549c517187db21eff35f4e9b3
Mike Avgeros <mike.avgeros@gmail.com>
2021-08-22 12:57:03 +0100

    Add jobs routes

---
+++ application/jobs/routes.py
@@ -0,0 +1,23 @@
+from flask import render_template, request, Blueprint
+
+
+jobs = Blueprint('jobs', __name__)
+
+
+# --------------- Jobs page ----------------
+@jobs.route("/jobs")
+def show_jobs():
+    return render_template("jobs.html")
+
+
+# --------------- Edit Job page ----------------
+@jobs.route("/edit_job")
+def edit_job():
+    return render_template("edit_job.html")
+
+
+# --------------- Upload New Job page ----------------
+@jobs.route("/upload_job")
+def upload_job():
+    return render_template("upload_job.html")
+

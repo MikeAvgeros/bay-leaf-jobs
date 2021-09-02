0008327bea0b757e5a7c7178425b9a9c41c8fe23
Mike Avgeros <mike.avgeros@gmail.com>
2021-09-02 12:57:08 +0100

    Update styling on pages. Add jobs on home page.

--- application/main/routes.py
+++ application/main/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, Blueprint, flash, redirect, url_for
 from application.main.forms import ContactForm
-from application.models import Contact
+from application.models import Job, Contact
 
 
 main = Blueprint('main', __name__, template_folder="templates")
@@ -11,7 +11,8 @@ main = Blueprint('main', __name__, template_folder="templates")
 @main.route("/home")
 @main.route("/index")
 def home():
-    return render_template("home.html")
+    jobs = Job.find_all_jobs()
+    return render_template("home.html", jobs=jobs)
 
 
 # --------------- Contact page ----------------
@@ -22,9 +23,10 @@ def contact():
     if form.validate_on_submit():
         name        = form.name.data.lower()
         email       = form.email.data.lower()
-        description = form.description.data
+        message     = form.message.data
+        user_id     = form.user_id.data
 
-        contact = Contact(name, email, description)
+        contact = Contact(name, email, message, user_id)
         contact.insert_into_database()
 
         flash("Request submitted successfully")

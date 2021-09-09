b476023399b75d3aa9fd166ec42a2a10b4b951e3
Mike Avgeros <mike.avgeros@gmail.com>
2021-09-09 14:28:00 +0100

    Remove unnecessary code.

--- application/static/js/script.js
+++ application/static/js/script.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
-    $(".collapsible").collapsible();
     $(".tooltipped").tooltip();
     $('select').formSelect();
-    $('.carousel').carousel();
 });

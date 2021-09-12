eba8eae377280f8de6ea40db00e2f8656133bfb0
Mike Avgeros <mike.avgeros@gmail.com>
2021-09-12 19:23:10 +0100

    Add confirm popup when deleting profile and job.

--- application/static/js/script.js
+++ application/static/js/script.js
@@ -8,15 +8,3 @@ function delete_flash() {
     const flash = document.querySelector('.flash-message');
     flash.remove();
 }
-
-function check_if_delete_confirmed() {
-    // modal appears
-
-    $.ajax({
-        url: "/",
-        type: 'POST',
-        data: {
-            
-        }
-    });
-}
